08fefba348cb2bbbc4c3f6004eddf252c7710aea
Andrey Zanko <alekseysayarkin@gmail.com>
2021-04-02 06:09:45 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,195 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FileComponent } from './core/components/file.component';
+import { FileService } from './core/services/file.service';
+import { TaskComponent } from './core/components/task.component';
+import { TaskService } from './core/services/task.service';
+import { saveAs } from 'file-saver';
+import { NgForm } from '@angular/forms';
+import { JwtClientService } from './core/services/jwt-client.service';
+import { JwtComponent } from './core/components/jwt.component';
+import { AuthRequestComponent } from './core/components/auth-request.component';
+import { SortComponent } from './core/components/sort.component';
+import { PaginationComponent } from './core/components/pagination.component';
+import {DateConstant} from './core/constants/date-constant';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularWeb';
+export class AppComponent implements OnInit{
+
+  public tasks: TaskComponent[];
+  public file: FileComponent;
+  public task: TaskComponent;
+  public pagination: PaginationComponent = new PaginationComponent();
+  public sort: SortComponent = new SortComponent();
+  private jwt: JwtComponent;
+  public hasFile: Boolean;
+  private deleteFile: Boolean = false;
+
+  constructor(private taskService: TaskService, private fileService: FileService, private jwtClientService: JwtClientService) {}
+
+  public ngOnInit() {
+    this.openModal(null, 'authenticate');
+  }
+
+  public openModal(task: TaskComponent, modalName: string): void {
+    const container = document.getElementById('container');
+
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+
+    switch(modalName) {
+      case 'add': {
+        button.setAttribute('data-target', '#addTaskModal');
+        break;
+      }
+      case 'update': {
+        this.task = task;
+        this.hasFile = task.file != null;
+        button.setAttribute('data-target', '#updateTaskModal');
+        break;
+      }
+      case 'delete': {
+        this.task = task;
+        button.setAttribute('data-target', '#deleteTaskModal');
+        break;
+      }
+      case 'authenticate': {
+        button.setAttribute('data-target', '#authenticateModal');
+        break;
+      }
+    }
+
+    container?.appendChild(button);
+    button.click();
+  }
+
+  public onDeleteFile(): void {
+    this.deleteFile = true;
+  }
+
+  public onChangeStatus(event: any) {
+    this.sort.statusFilter = event.target.value;
+    this.getTasks();
+  }
+
+  public authenticate(authForm: NgForm) {
+    document.getElementById('close-authentication-modal').click();
+
+    const authRequest = new AuthRequestComponent();
+    authRequest.login = authForm.controls['login'].value;
+    authRequest.password = authForm.controls['password'].value;
+
+    this.jwtClientService.generateToken(authRequest).subscribe(
+      (response: string) => {
+        this.jwt = new JwtComponent();
+        this.jwt.jwt = response;
+        this.getTasks();
+        this.setPagination();
+      },
+      () => {
+        alert("Error while signing up");
+        this.openModal(null, 'authenticate');
+      }
+    );
+  }
+
+  public getTasks(): void {
+    this.taskService.getTasks(this.sort, this.pagination, this.jwt).subscribe(
+      (response: TaskComponent[]) => {
+        this.tasks = response;
+        this.tasks.forEach(t => { t.endDate = t.endDate.substring(0, 10) })
+      },
+      (error: HttpErrorResponse) => { alert(error.message); }
+    )
+  }
+
+  public setPagination(): void {
+    this.taskService.getCount(this.jwt).subscribe(
+      (response: number) => {
+        this.pagination.count = response;
+        this.pagination.totalPages = Math.floor(((this.pagination.count - 1) / this.pagination.size) + 1);
+      },
+      (error: HttpErrorResponse) => { alert(error.message); }
+    );
+  }
+
+  public onDownloadFile(task: TaskComponent): void {
+    this.fileService.getFile(task.id, this.jwt).subscribe(
+      (response: Blob) => { saveAs(response, task.file?.name); },
+      (error: HttpErrorResponse) => { alert(error.message); }
+    );
+  }
+
+  public loadFile(event: any)  {
+    this.file = new FileComponent();
+    const uploadedFile: File = <File> event.target.files[0];
+    this.file.name = uploadedFile.name;
+    this.file.data = uploadedFile;
+  }
+
+  public onAddTask(addForm: NgForm): void {
+    document.getElementById('close-add-task').click();
+
+    this.taskService.addTask(this.extractFromForm(addForm), this.jwt).subscribe(
+      () => {}, (error: HttpErrorResponse) => { alert(error.message); }
+    );
+    this.getTasks()
+  }
+
+  private extractFromForm(form: NgForm): TaskComponent {
+    const task = new TaskComponent();
+    const taskName = form.controls['task'].value;
+    const taskEndDate = form.controls['end_date'].value;
+
+    if (taskName != '')
+      task.task = taskName;
+    else
+      task.task = this.task.task;
+
+    if (taskEndDate != '')
+      task.endDate = taskEndDate + DateConstant.DATE_CONSTANT_POSTFIX;
+    else
+      task.endDate = this.task.endDate + DateConstant.DATE_CONSTANT_POSTFIX;
+
+    task.file = this.file;
+    this.file = null;
+
+    return task;
+  }
+
+  public onUpdateTask(updateForm: NgForm): void {
+    document.getElementById('close-update-task').click();
+
+    const task = this.extractFromForm(updateForm);
+    task.id = this.task.id;
+    task.status = this.task.status;
+    if (this.deleteFile) {
+      task.file = null;
+      this.deleteFile = false;
+    }
+    this.taskService.updateTask(task, this.jwt).subscribe(() => {}, (error: HttpErrorResponse) => { alert(error.message); });
+    this.delay(1000).then(() => { this.getTasks() });
+  }
+
+  public changePage(page: number): void {
+    if (page <= this.pagination.totalPages && page > 0) {
+      this.pagination.page = page;
+      this.getTasks();
+    }
+  }
+
+  public onDeleteTask(id: number): void {
+    this.taskService.deleteTask(id, this.jwt);
+    this.delay(1000).then(() => { this.getTasks() });
+  }
+
+  async delay(ms: number) {
+    await new Promise<void>(resolve => setTimeout(() => resolve(), ms));
+  }
 }
---
+++ src/app/core/constants/date-constant.ts
@@ -0,0 +1,3 @@
+export const DateConstant = {
+  DATE_CONSTANT_POSTFIX: "T00:00:00+03:00"
+};
